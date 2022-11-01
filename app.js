5625f5abf76c3f16de81b48aa70d7fad0af068d5
[raj nishad] <[rajnishad24082003@gmail.com]>
2022-11-01 23:17:15 +0530

    heroku file added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000 || process.env ;
+const port = 3000 || process.env.PORT ;
 const userRoutes = require("./router/routes");
 const mongocon =  require("./db/connect");
 require("dotenv").config();
